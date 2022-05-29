96d0a55ca54f4dd9cc7ea3b8033c3fcb698337a1
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-05-29 07:43:49 -0300

    Criação de arquivo de ambiente de exemplo, configs e módulo de orders

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) { }
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    console.log(createOrderDto)
+    return createOrderDto
+  }
+
+  @Get()
+  findAll() {
+    return { res: 'ok' }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersService.findOne(+id);
+  }
+}
